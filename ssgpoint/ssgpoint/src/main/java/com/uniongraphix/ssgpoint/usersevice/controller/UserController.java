b239047bb59a33c65e1b873155ef79f12b6fa02d
Jason Ahn <42033861+beat1103@users.noreply.github.com>
2023-08-03 11:26:31 +0900

    feat: user controller

--- ssgpoint/ssgpoint/src/main/java/com/uniongraphix/ssgpoint/usersevice/controller/UserController.java
+++ ssgpoint/ssgpoint/src/main/java/com/uniongraphix/ssgpoint/usersevice/controller/UserController.java
@@ -1,5 +1,30 @@
 package com.uniongraphix.ssgpoint.usersevice.controller;
 
-public class UserController {
+import com.uniongraphix.ssgpoint.usersevice.entity.User;
+import com.uniongraphix.ssgpoint.usersevice.service.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class UserController {
+    private final IUserService userService;
+
+    @PostMapping("/user")
+    public String createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
 }
