5fa2c243d9b27efd5b29f2b9a810c71765bbf19e
Jason Ahn <42033861+beat1103@users.noreply.github.com>
2023-09-01 09:39:42 +0900

    feat: NextAuth kakao Login and Naver login

--- ssg-point-client/app/api/auth/[...nextauth]/options.ts
+++ ssg-point-client/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,7 @@
 import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import KakaoProvider from "next-auth/providers/kakao";
+import NaverProvider from "next-auth/providers/naver";
 
 export const options: NextAuthOptions = {
   providers: [
@@ -33,6 +35,14 @@ export const options: NextAuthOptions = {
         // Return null if user data could not be retrieved
         return null
       }
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID,
+      clientSecret: process.env.NAVER_CLIENT_SECRET
     })
   ],
 
--- ssg-point-client/app/login/page.tsx
+++ ssg-point-client/app/login/page.tsx
@@ -3,6 +3,7 @@ import Logo from '@/components/ui/header/Logo'
 import { getServerSession } from 'next-auth'
 import React from 'react'
 import { options } from '../api/auth/[...nextauth]/options'
+import OAuthLogin from '@/components/page/login/OAuthLogin'
 // get path from 'path'
 
 async function Login() {
@@ -15,6 +16,7 @@ async function Login() {
     <section className='flex flex-col items-center gap-10 pt-20'>
       <Logo url={'/'} imgAlt={'Login Logo'} />
       <LoginForm />
+      <OAuthLogin />
     </section>
   )
 }
